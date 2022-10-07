cfcfb36792b85c72e59fde43cc60bc755ebd8393
Deti Baholli <detibaholli@gmail.com>
2022-10-08 01:16:09 +0200

    Video Migrations

---
+++ Migrations/20221007214151_Video-Description.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace elefanti.video.backend.Migrations
+{
+    public partial class VideoDescription : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Videos",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Videos");
+        }
+    }
+}
